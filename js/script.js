b3247ac18b6b7eb01445b58c21d867fa00b4ac36
Sarah <sarahelhefnawy0@gmail.com>
2025-03-27 15:30:20 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const element = document.getElementById("element");
+
+var typed = new Typed(element, {
+    strings: ['Designer', 'Developer'],
+    typeSpeed: 45,
+    backSpeed: 80,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+});
